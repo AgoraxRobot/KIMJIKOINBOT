da4a2e6d566a0826170e2f3098fe58a506156112
Team Agora <132120521+TeamAgora@users.noreply.github.com>
2023-05-22 22:29:32 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -45,7 +45,7 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/8c78951f44aa036252af9.jpg"
+        "START_IMG", "https://te.legra.ph/file/c099aa329306ef7df3cf8.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "teamagora")
